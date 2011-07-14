91280937b973df5afadb75ef6629e9dd680f112c
Thomas Gleixner <tglx@linutronix.de>
2011-07-14 09:56:44 +0200

    sched: Take RT softirq semantics into account in cond_resched()
    The softirq semantics work different on -RT. There is no SOFTIRQ_MASK in
    the preemption counter which leads to the BUG_ON() statement in
    __cond_resched_softirq(). As for -RT it is enough to perform a "normal"
    schedule.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -4861,6 +4861,7 @@ int __cond_resched_lock(spinlock_t *lock)
 }
 EXPORT_SYMBOL(__cond_resched_lock);
 
+#ifndef CONFIG_PREEMPT_RT_FULL
 int __sched __cond_resched_softirq(void)
 {
 	BUG_ON(!in_softirq());
@@ -4874,6 +4875,7 @@ int __sched __cond_resched_softirq(void)
 	return 0;
 }
 EXPORT_SYMBOL(__cond_resched_softirq);
+#endif
 
 /**
  * yield - yield the current processor to other threads.
