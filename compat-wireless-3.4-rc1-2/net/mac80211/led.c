901c2ef13694dece2bfb7746ee5ce6f0e79ebf82
Julien Massot <jmassot@aldebaran-robotics.com>
2012-04-06 09:59:46 +0000

    remove led_brightness_set

--- compat-wireless-3.4-rc1-2/net/mac80211/led.c
+++ compat-wireless-3.4-rc1-2/net/mac80211/led.c
@@ -269,11 +269,6 @@ static void ieee80211_stop_tpt_led_trig(struct ieee80211_local *local)
 
 	tpt_trig->running = false;
 	del_timer_sync(&tpt_trig->timer);
-
-	read_lock(&tpt_trig->trig.leddev_list_lock);
-	list_for_each_entry(led_cdev, &tpt_trig->trig.led_cdevs, trig_list)
-		led_brightness_set(led_cdev, LED_OFF);
-	read_unlock(&tpt_trig->trig.leddev_list_lock);
 }
 
 void ieee80211_mod_tpt_led_trig(struct ieee80211_local *local,
