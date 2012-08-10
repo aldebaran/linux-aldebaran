0a9106f0efd76d573a073142bff1c3453e3f5e7c
Julien Massot <jmassot@aldebaran-robotics.com>
2012-08-10 16:44:24 +0200

    rt2800: disable powersaving

--- compat-wireless-3.2-1/drivers/net/wireless/rt2x00/rt2800lib.c
+++ compat-wireless-3.2-1/drivers/net/wireless/rt2x00/rt2800lib.c
@@ -4198,8 +4198,8 @@ int rt2800_probe_hw_mode(struct rt2x00_dev *rt2x00dev)
 	/*
 	 * Disable powersaving as default on PCI devices.
 	 */
-	if (rt2x00_is_pci(rt2x00dev) || rt2x00_is_soc(rt2x00dev))
-		rt2x00dev->hw->wiphy->flags &= ~WIPHY_FLAG_PS_ON_BY_DEFAULT;
+
+	rt2x00dev->hw->wiphy->flags &= ~WIPHY_FLAG_PS_ON_BY_DEFAULT;
 
 	/*
 	 * Initialize all hw fields.
