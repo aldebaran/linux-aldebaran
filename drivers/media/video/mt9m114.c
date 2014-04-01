7498999bd742acae6253b1e555f1e10980aa1b77
clemolgat <clemolgat@aldebaran-robotics.com>
2014-04-01 17:45:13 +0200

    MT9M114: Fix contrast range
    - according to mt9m114 RR revE data sheet 0xCC0C is between [16;64]
    
    Change-Id: I11fc0656c7ccf227c3df7965742e103250259798
    Reviewed-on: http://gerrit.aldebaran.lan/38312
    Tested-by: clemolgat <clemolgat@aldebaran-robotics.com>
    Reviewed-by: clemolgat <clemolgat@aldebaran-robotics.com>

--- drivers/media/video/mt9m114.c
+++ drivers/media/video/mt9m114.c
@@ -2086,7 +2086,7 @@ static int mt9m114_queryctrl(struct v4l2_subdev *sd,
     case V4L2_CID_BRIGHTNESS:
       return v4l2_ctrl_query_fill(qc, 0, 255, 1, 55);
     case V4L2_CID_CONTRAST:
-      return v4l2_ctrl_query_fill(qc, 0, 127, 1, 32);
+      return v4l2_ctrl_query_fill(qc, 16, 64, 1, 32);
     case V4L2_CID_SATURATION:
       return v4l2_ctrl_query_fill(qc, 0, 255, 1, 128);
     case V4L2_CID_HUE:
