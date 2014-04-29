83a47c8c1ef7510336a573d34d2b6449cba130fd
lguegan <lguegan@aldebaran-robotics.com>
2014-04-29 11:12:20 +0200

    ath6kl: remove MATCH_SSID_FLAG to make hidden network working
    commit 558a64497bffe6bdc7f2af51a3d22d1472f91640
    Author: Julien Massot <jmassot@aldebaran-robotics.com>
    Date:   Mon Feb 18 10:43:15 2013 +0100
    
        compat: ath6kl remove MATCH_SSID_FLAG to make hidden network working
    
    Change-Id: I36e55add8347402b9a116b72f510c6aacd1805a2
    Reviewed-on: http://gerrit.aldebaran.lan/42389
    Reviewed-by: jmassot <jmassot@aldebaran-robotics.com>
    Tested-by: jmassot <jmassot@aldebaran-robotics.com>
    Approved: nrubinstein <nrubinstein@aldebaran-robotics.com>
    Reviewed-by: nrubinstein <nrubinstein@aldebaran-robotics.com>
    Approved: lguegan <lguegan@aldebaran-robotics.com>
    Reviewed-by: lguegan <lguegan@aldebaran-robotics.com>
    Tested-by: lguegan <lguegan@aldebaran-robotics.com>

--- drivers/net/wireless/ath/ath6kl/cfg80211.c
+++ drivers/net/wireless/ath/ath6kl/cfg80211.c
@@ -932,9 +932,6 @@ static int ath6kl_set_probed_ssids(struct ath6kl *ar,
 			ssid_list[i].flag = SPECIFIC_SSID_FLAG;
 		else
 			ssid_list[i].flag = ANY_SSID_FLAG;
-
-		if (n_match_ssid == 0)
-			ssid_list[i].flag |= MATCH_SSID_FLAG;
 	}
 
 	index_to_add = i;
@@ -948,7 +945,6 @@ static int ath6kl_set_probed_ssids(struct ath6kl *ar,
 			    (!memcmp(ssid_list[j].ssid.ssid,
 				     match_set[i].ssid.ssid,
 				     match_set[i].ssid.ssid_len))) {
-				ssid_list[j].flag |= MATCH_SSID_FLAG;
 				ssid_found = true;
 				break;
 			}
@@ -965,7 +961,6 @@ static int ath6kl_set_probed_ssids(struct ath6kl *ar,
 		memcpy(ssid_list[index_to_add].ssid.ssid,
 		       match_set[i].ssid.ssid,
 		       match_set[i].ssid.ssid_len);
-		ssid_list[index_to_add].flag |= MATCH_SSID_FLAG;
 		index_to_add++;
 	}
 
