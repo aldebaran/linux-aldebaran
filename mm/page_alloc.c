07ce01ebeeb43b5e7d51748c476eb630e4d4c4b2
Julien Massot <jmassot@aldebaran-robotics.com>
2015-06-24 13:30:09 +0200

    Log direct pages reclaim
    Change-Id: I9cc1c4c7de02a138a756537e248f6d2d6917c236

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -2952,6 +2952,8 @@ __perform_reclaim(gfp_t gfp_mask, unsigned int order,
 	struct reclaim_state reclaim_state;
 	int progress;
 
+	printk(KERN_ERR "__alloc_pages_direct_reclaim");
+
 	cond_resched();
 
 	/* We now go into synchronous reclaim */
