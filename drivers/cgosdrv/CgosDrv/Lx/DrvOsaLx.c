d9bc8ec2bccaa01b7c223170abbc26fdbeea4fd9
Ludovic Guegan <lguegan@aldebaran-robotics.com>
2017-02-10 15:13:18 +0100

    Revert "drivers: cleanup cgosdrv directory"
    This reverts commit d5c7da385894fc9266a2dec65a7c69d798bc08f9.
    
    Change-Id: Id779486fabb8d5caa2798626906e278aa55fba5a
    Reviewed-on: http://gerrit.aldebaran.lan/80464
    Reviewed-by: nrubinstein <nrubinstein@aldebaran-robotics.com>
    Tested-by: lguegan <lguegan@aldebaran-robotics.com>

--- drivers/cgosdrv/CgosDrv/Lx/DrvOsaLx.c
+++ drivers/cgosdrv/CgosDrv/Lx/DrvOsaLx.c
@@ -3,25 +3,25 @@
  * Copyright (c) 2015, congatec AG. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; either version 2 of
+ * modify it under the terms of the GNU General Public License as 
+ * published by the Free Software Foundation; either version 2 of 
  * the License, or (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * This program is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
  * See the GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software Foundation,
+ * along with this program; if not, write to the Free Software Foundation, 
  * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  *
- * The full text of the license may also be found at:
+ * The full text of the license may also be found at:        
  * http://opensource.org/licenses/GPL-2.0
  *
  *---------------------------------------------------------------------------
- */
-
+ */ 
+ 
 //***************************************************************************
 
 #include <linux/version.h>
@@ -43,9 +43,9 @@
 #endif
 
 //MODGWE #define cgos_cdecl __attribute__((regparm(0)))
-#define cgos_cdecl	//MODGWE
+#define cgos_cdecl	//MODGWE 
 
-#include "DrvOsa.h"
+#include <DrvOsa.h>
 
 //***************************************************************************
 
