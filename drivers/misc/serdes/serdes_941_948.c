f8bdf00beb3ecae2389f5273e7fb2fa53f890679
Michelle Le Grand <michelle.legrand@softbankrobotics.com>
2020-01-20 11:32:40 +0100

    screen: add DSI support for Tessolve screen
    Change-Id: I6d36bd80c657cc24ea22d5c6cf11d4dc04a8f160

--- drivers/misc/serdes/serdes_941_948.c
+++ drivers/misc/serdes/serdes_941_948.c
@@ -13,19 +13,21 @@
 #define REG_SLAVE_ID_0		0x07
 #define REG_SLAVE_ID_1		0x70
 #define REG_SLAVE_ID_2		0x71
+#define REG_SLAVE_ID_3		0x72
 
 #define REG_SLAVE_ALIAS_0	0x08
 #define REG_SLAVE_ALIAS_1	0x77
 #define REG_SLAVE_ALIAS_2	0x78
+#define REG_SLAVE_ALIAS_3	0x79
 
 
 #define REMOTE_SLAVE_ADD_1	0x50	//NFC
 #define REMOTE_SLAVE_ADD_2	0x78	//Backlight
-#define REMOTE_SLAVE_ADD_3	0xBA	//changed to BA since slave address is changed to 0x5d from 0x14
+#define REMOTE_SLAVE_ADD_3	0xBA	//TOUCH: changed to BA since slave address is changed to 0x5d from 0x14
 #define REMOTE_SLAVE_ADD_4	0xA0	//EEPROM
 
-#define SERDES_ADDRESS_1	0x0C
-#define SERDES_ADDRESS_2	0x2C
+#define SER_ADDRESS	0x0C
+#define DES_ADDRESS	0x2C
 
 struct serdes_941_948_data {
     struct i2c_client *client;
@@ -35,41 +37,56 @@ struct serdes_941_948_data {
 static int serdes_941_init(struct serdes_941_948_data *data)
 {
     //ds90ub941  Device initialization
-    pr_info("%s : %d SERDES \n",__func__, __LINE__);
+    pr_info("%s\n",__func__);
 
     //Remote slave devices
     i2c_smbus_write_byte_data(data->client, REG_SLAVE_ID_0,    REMOTE_SLAVE_ADD_1);
     i2c_smbus_write_byte_data(data->client, REG_SLAVE_ALIAS_0, REMOTE_SLAVE_ADD_1);
     i2c_smbus_write_byte_data(data->client, REG_SLAVE_ID_1,    REMOTE_SLAVE_ADD_2);
     i2c_smbus_write_byte_data(data->client, REG_SLAVE_ALIAS_1, REMOTE_SLAVE_ADD_2);
-    i2c_smbus_write_byte_data(data->client, REG_SLAVE_ID_2,	   REMOTE_SLAVE_ADD_3);
+    i2c_smbus_write_byte_data(data->client, REG_SLAVE_ID_2,    REMOTE_SLAVE_ADD_3);
     i2c_smbus_write_byte_data(data->client, REG_SLAVE_ALIAS_2, REMOTE_SLAVE_ADD_3);
 
 
-    i2c_smbus_write_byte_data(data->client, 0x72, REMOTE_SLAVE_ADD_4);
-    i2c_smbus_write_byte_data(data->client, 0x79, REMOTE_SLAVE_ADD_4);
+    i2c_smbus_write_byte_data(data->client, REG_SLAVE_ID_3, REMOTE_SLAVE_ADD_4);
+    i2c_smbus_write_byte_data(data->client, REG_SLAVE_ALIAS_3, REMOTE_SLAVE_ADD_4);
 
-    //EEPROM for EDID
-    i2c_smbus_write_byte_data(data->client, 0x51, REMOTE_SLAVE_ADD_4);
-    i2c_smbus_write_byte_data(data->client, 0x03, 0xDA);
+    //GENERAL_CFG
+    i2c_smbus_write_byte_data(data->client, 0x03, 0x9A);
+
+    //Disable DSI
+    //i2c_smbus_write_byte_data(data->client, 0x01, 0x08);
+
+    //Disable I2S
+    i2c_smbus_write_byte_data(data->client, 0x54, 0x00);
 
     //GPIO config
-    i2c_smbus_write_byte_data(data->client, 0x0f, 0x03);
-    i2c_smbus_write_byte_data(data->client, 0x0E, 0x35);
-    i2c_smbus_write_byte_data(data->client, 0x0D, 0x05);
+    i2c_smbus_write_byte_data(data->client, 0x0F, 0x03);// GPIO 3
+    i2c_smbus_write_byte_data(data->client, 0x0E, 0x35);// GPIO 2 and 1
+    i2c_smbus_write_byte_data(data->client, 0x0D, 0x05);// GPIO 0
 
     return 0;
 }
 
 static int serdes_948_init(struct serdes_941_948_data *data)
 {
-    pr_info("%s : %d SERDES \n",__func__, __LINE__);
-    //ds90ub941  Device initialization
-    //i2c_smbus_write_byte_data(data->client, 0x03, 0xF8);
+    //ds90ub948  Device initialization
+    pr_info("%s\n",__func__);
+
+    //GENERAL_CONFIGURATION_1
+    i2c_smbus_write_byte_data(data->client, 0x03, 0xF8);
+
+    //I2C_CONTROL_1
     i2c_smbus_write_byte_data(data->client, 0x05, 0x9E);
-    i2c_smbus_write_byte_data(data->client, 0x1F, 0x05);
-    i2c_smbus_write_byte_data(data->client, 0x1E, 0x53);
-    i2c_smbus_write_byte_data(data->client, 0x1D, 0x03);
+
+    //GPIO config
+    i2c_smbus_write_byte_data(data->client, 0x1F, 0x05);// GPIO 3
+    i2c_smbus_write_byte_data(data->client, 0x1E, 0x53);// GPIO 2 and 1
+    i2c_smbus_write_byte_data(data->client, 0x1D, 0x03);// GPIO 0
+
+    //CML_OUTPUT
+    i2c_smbus_write_byte_data(data->client, 0x56, 0x08);
+    i2c_smbus_write_byte_data(data->client, 0x57, 0x02);
 
     return 0;
 
@@ -91,7 +108,7 @@ static int serdes_941_948_probe(struct i2c_client *client,
 
     //read register 0x00 if value is 0x18 call serdes_941_init() else if 0x58 call serdes_948_init();
     serdes_941_init(data);
-    data->client->addr = SERDES_ADDRESS_2;
+    data->client->addr = DES_ADDRESS;
     serdes_948_init(data);
 
     return 0;
